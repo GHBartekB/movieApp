e02de07fadc5990268ee2d79724670b6b6cf1ca6
Bartek Brychcy <bbrychcy@onet.eu>
2021-03-17 14:15:38 +0100

    add page=1 to API_URL

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,7 +2,7 @@ const form = document.querySelector('.header__form');
 const input = document.querySelector('.header__input');
 
 const API_KEY = '4c17b0f0e8818fa255c7f299e421fe9a';
-const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}`;
+const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1`;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key${API_KEY}&query="`;
 
@@ -17,3 +17,4 @@ const getData = (e) => {
 
 form.addEventListener('submit', getData);
 
+console.log(API_URL)
