6ee5898137e372fb9dc685de6758776c34e3207f
Bartek Brychcy <bbrychcy@onet.eu>
2021-03-19 08:53:42 +0100

    change SEARCH_URL add changes to getData function and fetchData

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,10 +4,10 @@ const input = document.querySelector('.header__input');
 const API_KEY = '4c17b0f0e8818fa255c7f299e421fe9a';
 const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1`;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key${API_KEY}&query="`;
+const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key${API_KEY}&query=`;
 
-const fetchData = () => {
-    fetch(API_URL)
+const fetchData = (URL) => {
+    fetch(URL)
     .then(response => {
         if(response.ok) {
             return response;
@@ -23,24 +23,27 @@ const fetchData = () => {
     .catch(error => console.log(error + "something went wrong"))
 }
 
+fetchData(API_URL);
 
 
 const createElement = (moviesData) => {
     const movies = document.querySelector('.movies');
+    movies.innerHTML = '';
     moviesData.map(movie => {
         const element = document.createElement('div');
         const image = IMG_URL + movie.poster_path;
+        const {title, rate, overview} = movie;
         element.classList.add('movie');
         element.innerHTML = ` 
         <img class="movie__image"
                         src=${image}
-                        alt=${movie.title}>
+                        alt=${title}>
                     <div class="movie__info">
-                        <p class="movie__title">${movie.title}</p>
-                        <span class="movie__rate">${movie.rate}</span>
+                        <p class="movie__title">${title}</p>
+                        <span class="movie__rate">${rate}</span>
                     </div>
                     <p class="movie__description">
-                        ${movie.overview}
+                        ${overview}
                     </p>
     `;
         movies.appendChild(element);
@@ -52,7 +55,9 @@ const createElement = (moviesData) => {
 const getData = (e) => {
     e.preventDefault();
     const value = input.value;
-    fetchData();
+    const URL = SEARCH_URL + value;
+    console.log(URL);
+    fetchData(URL);
 
     input.value = "";
 
